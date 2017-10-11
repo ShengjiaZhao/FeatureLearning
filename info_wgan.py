Fix incorrect kl

info_wgan.py
@@ -142,8 +142,8 @@ class GenerativeAdversarialNet(object):
         self.wake_kl_loss = tf.reduce_mean(tf.reduce_sum(self.wake_kl_loss, axis=1))
 
         self.sleep_zmean, self.sleep_zstddev = inference(self.g, self.z_dim, reuse=True)
-        self.sleep_kl_loss = -tf.log(self.wake_zstddev) + 0.5 * tf.square(self.wake_zstddev) + 0.5 * tf.square(self.wake_zmean) - 0.5
-        self.sleep_kl_loss = tf.reduce_mean(tf.reduce_sum(-self.sleep_kl_loss, axis=1))
+        self.sleep_kl_loss = tf.log(self.wake_zstddev) + 0.5 / tf.square(self.wake_zstddev) + 0.5 * tf.square(self.wake_zmean) / tf.square(self.wake_zstddev) - 0.5
+        self.sleep_kl_loss = tf.reduce_mean(tf.reduce_sum(self.sleep_kl_loss, axis=1))
         self.i_loss = self.wake_kl_loss * args.ratio + self.sleep_kl_loss
 
         self.d_vars = [var for var in tf.global_variables() if 'd_net' in var.name]